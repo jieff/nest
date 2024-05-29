c3d546ff84ab6a0d8b4f34df02faeb178e8bba74
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2024-05-29 10:10:20 -0300

    test: deploy

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -12,7 +12,7 @@ export class WishesService{
     async getAll(): Promise<Wish[]> {
         return this.wishModel.find().exec();
       }
-    //teste
+    //teste 3
       async create(wish: Wish): Promise<Wish> {
         const wishCreate = new this.wishModel(wish);
     
